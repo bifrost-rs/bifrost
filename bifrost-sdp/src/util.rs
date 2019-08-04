b759910b813210d45106e93fbb7071a50bfaf285
Zizheng Tai <me@zizheng.me>
2019-08-04 06:45:44 -0700

    Address Clippy warnings

--- bifrost-sdp/src/util.rs
+++ bifrost-sdp/src/util.rs
@@ -3,7 +3,7 @@ use nom::character::complete::{line_ending, not_line_ending};
 use nom::combinator::verify;
 use nom::IResult;
 
-pub fn parse_nonempty_line<'a>(type_tag: &'a str) -> impl Fn(&'a str) -> IResult<&'a str, &'a str> {
+pub fn parse_nonempty_line(type_tag: &str) -> impl Fn(&str) -> IResult<&str, &str> + '_ {
     move |input| {
         let (rest, _) = tag(type_tag)(input)?;
         let (rest, value) = verify(not_line_ending, |s: &str| !s.is_empty())(rest)?;
