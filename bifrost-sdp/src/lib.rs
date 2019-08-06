0f9970f62793c5bba07d57e350ee43990373a311
Zizheng Tai <me@zizheng.me>
2019-08-06 13:04:24 -0700

    Format

--- bifrost-sdp/src/lib.rs
+++ bifrost-sdp/src/lib.rs
@@ -1,40 +1,28 @@
-// Expose only the `SessionDescription` type.
-mod session_description;
-pub use self::session_description::SessionDescription;
-
 mod email_address;
-use self::email_address::EmailAddress;
-
 mod information;
-use self::information::Information;
-
 mod origin;
-use self::origin::Origin;
-
 mod phone_number;
-use self::phone_number::PhoneNumber;
-
+mod session_description;
 mod session_name;
-use self::session_name::SessionName;
-
 mod uri;
-use self::uri::Uri;
-
-mod version;
-use self::version::Version;
-
 mod util;
+mod version;
 
-use nom::IResult;
+pub use crate::session_description::SessionDescription;
+
+use crate::{
+    email_address::EmailAddress, information::Information, origin::Origin,
+    phone_number::PhoneNumber, session_name::SessionName, uri::Uri, version::Version,
+};
 
 trait Parse {
-    fn parse(input: &str) -> IResult<&str, Self>
+    fn parse(input: &str) -> nom::IResult<&str, Self>
     where
         Self: Sized;
 }
 
 impl<T: Parse> Parse for Option<T> {
-    fn parse(input: &str) -> IResult<&str, Self> {
+    fn parse(input: &str) -> nom::IResult<&str, Self> {
         nom::combinator::opt(T::parse)(input)
     }
 }
