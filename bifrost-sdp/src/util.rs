f00ac6eb48d43abd6c3f5c030f1a7e220438af5f
Zizheng Tai <me@zizheng.me>
2019-08-16 17:28:18 -0700

    Simplify util function

--- bifrost-sdp/src/util.rs
+++ bifrost-sdp/src/util.rs
@@ -1,14 +1,14 @@
 use std::str::FromStr;
 
 use nom::bytes::complete::{is_not, tag};
-use nom::character::complete::{line_ending, not_line_ending};
-use nom::combinator::{map_res, verify};
+use nom::character::complete::line_ending;
+use nom::combinator::map_res;
 use nom::IResult;
 
 pub fn parse_nonempty_line(type_tag: &str) -> impl Fn(&str) -> IResult<&str, &str> + '_ {
     move |input| {
         let (rest, _) = tag(type_tag)(input)?;
-        let (rest, value) = verify(not_line_ending, |s: &str| !s.is_empty())(rest)?;
+        let (rest, value) = is_not("\r\n")(rest)?;
         let (rest, _) = line_ending(rest)?;
         Ok((rest, value))
     }
