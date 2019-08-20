13315a959f139d9a85b60e06eb77300990e8d332
Zizheng Tai <me@zizheng.me>
2019-08-20 14:22:11 -0700

    Implement Display for SessionName

--- bifrost-sdp/src/session_name.rs
+++ bifrost-sdp/src/session_name.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use nom::combinator::map;
 use nom::IResult;
 
@@ -7,6 +9,12 @@ use crate::{util, Parse};
 /// [RFC 4566](https://tools.ietf.org/html/rfc4566#section-5.3).
 #[derive(Clone, Debug, PartialEq)]
 pub struct SessionName(pub String);
+
+impl fmt::Display for SessionName {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "s={}\r", self.0)
+    }
+}
 
 impl Parse for SessionName {
     fn parse(input: &str) -> IResult<&str, Self> {
@@ -20,25 +28,27 @@ impl Parse for SessionName {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::test_util::{assert_err, assert_parse_display};
 
     #[test]
-    fn test_valid_1() {
-        let s = "s=test\r\nrest\n";
-        let (rest, SessionName(session_name)) = SessionName::parse(s).unwrap();
-        assert_eq!(rest, "rest\n");
-        assert_eq!(session_name, "test");
+    fn test_valid() {
+        assert_parse_display(
+            "s=test\r\nrest\n",
+            "rest\n",
+            &SessionName("test".to_owned()),
+            "s=test\r\n",
+        );
+
+        assert_parse_display(
+            "s= \nmore\r\n",
+            "more\r\n",
+            &SessionName(" ".to_owned()),
+            "s= \r\n",
+        );
     }
 
     #[test]
-    fn test_valid_2() {
-        let s = "s= \nmore\r\n";
-        let (rest, SessionName(session_name)) = SessionName::parse(s).unwrap();
-        assert_eq!(rest, "more\r\n");
-        assert_eq!(session_name, " ");
-    }
-
-    #[test]
-    fn test_empty() {
-        assert!(SessionName::parse("s=\r\n").is_err());
+    fn test_invalid() {
+        assert_err::<SessionName>("s=\r\n");
     }
 }
