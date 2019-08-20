9c726d94455d48c67fbc9ee0e5877320557a349c
Zizheng Tai <me@zizheng.me>
2019-08-19 23:14:18 -0700

    Clean up Parse impls

--- bifrost-sdp/src/parse.rs
+++ bifrost-sdp/src/parse.rs
@@ -1,4 +1,4 @@
-use nom::combinator::{map_res, opt};
+use nom::combinator::opt;
 use nom::multi::{many0, many1};
 use nom::IResult;
 use vec1::Vec1;
@@ -21,12 +21,17 @@ impl<T: Parse> Parse for Vec<T> {
 
 impl<T: Parse> Parse for Vec1<T> {
     fn parse(input: &str) -> IResult<&str, Self> {
-        map_res(many1(T::parse), Self::try_from_vec)(input)
+        let (rest, vec) = many1(T::parse)(input)?;
+        Ok((rest, Self::try_from_vec(vec).unwrap()))
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use nom::bytes::complete::tag;
+    use nom::character::complete::digit1;
+    use nom::combinator::map_res;
+
     use super::*;
 
     #[derive(Debug, PartialEq)]
@@ -34,9 +39,6 @@ mod tests {
 
     impl Parse for Test {
         fn parse(input: &str) -> IResult<&str, Self> {
-            use nom::bytes::complete::tag;
-            use nom::character::complete::digit1;
-
             let (rest, x) = map_res(digit1, str::parse)(input)?;
             let (rest, _) = tag(".")(rest)?;
 
