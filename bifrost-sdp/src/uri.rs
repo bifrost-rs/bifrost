eebfaf4c9caa119184dc4d21d97d2087e3c3eeec
Zizheng Tai <me@zizheng.me>
2019-08-20 02:24:50 -0700

    Implement Display for Uri

--- bifrost-sdp/src/uri.rs
+++ bifrost-sdp/src/uri.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use http::Uri as HttpUri;
 use nom::combinator::{map, map_res};
 use nom::IResult;
@@ -9,6 +11,12 @@ use crate::{util, Parse};
 #[derive(Clone, Debug, PartialEq)]
 pub struct Uri(pub HttpUri);
 
+impl fmt::Display for Uri {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "u={}\r", self.0)
+    }
+}
+
 impl Parse for Uri {
     fn parse(input: &str) -> IResult<&str, Self> {
         // u=<uri>
@@ -18,20 +26,23 @@ impl Parse for Uri {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{HttpUri, Uri};
+    use crate::test_util::{assert_err, assert_parse_display};
 
     #[test]
     fn test_valid() {
         let uri_str = "http://www.example.com/seminars/sdp.pdf";
-        let line = format!("u={}\r\nrest\n", uri_str);
-        let (rest, uri) = Uri::parse(&line).unwrap();
-        assert_eq!(rest, "rest\n");
-        assert_eq!(uri, Uri(HttpUri::from_static(uri_str)));
+        assert_parse_display(
+            &format!("u={}\r\nrest\n", uri_str),
+            "rest\n",
+            &Uri(HttpUri::from_static(uri_str)),
+            &format!("u={}\r\n", uri_str),
+        );
     }
 
     #[test]
     fn test_invalid() {
-        assert!(Uri::parse("u=\r\n").is_err());
-        assert!(Uri::parse("u= \r\n").is_err());
+        assert_err::<Uri>("u=\r\n");
+        assert_err::<Uri>("u= \r\n");
     }
 }
